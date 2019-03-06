5e5f72ed834e510b8a0ddea8ae7ca92407b71b92
claudio melendres <claudiomelendres@gmail.com>
2019-03-05 21:29:10 -0400

    second

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,24 +1,27 @@
 var socket = io();
 
-var params = new URLSearchParams( window.location.search );
+var params = new URLSearchParams(window.location.search);
 
-if ( !params.has('nombre') || !params.has('sala')) {
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y la sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
 
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function( resp ){
-        console.log('Usuarios conectados ', resp );
-    } );
+    socket.emit('entrarChat', usuario, function(resp) {
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
+    });
+
 });
 
 // escuchar
@@ -26,35 +29,35 @@ socket.on('disconnect', function() {
 
     console.log('Perdimos conexión con el servidor');
 
-   
-
 });
 
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Cayo',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
 // });
 
-// Escuchar información
+// Escuchar información mensaje desde el servidor u otros usuarios
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-//Escuchar cuando un usuario entra o sale del chat
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log('listaPersona: ',personas);
+    //console.log(personas);
+    renderizarUsuarios(personas); // para detectar cambios si un usuario entra o sale
 
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
+
     console.log('Mensaje Privado:', mensaje);
-})
+
+});
